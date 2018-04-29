9e52edfa0da6bc5f62b6df7d291caf56a4f8e202
mnao305 <naomasa305@gmail.com>
2018-04-29 16:27:08 +0900

    [add]TwitterAPI用のコンフィグ設定

---
+++ config/twitter.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'consumer_key' => env('TWITTER_CONSUMER_KEY', ''),
+    'consumer_secret' => env('TWITTER_CONSUMER_SECRET', ''),
+    'callback_url' => env('TWITTER_CALLBACK_URL', ''),
+];
