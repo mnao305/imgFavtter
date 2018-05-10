7869eb21f89e6ad357f3e6b4817bb113bd00aa6a
mnao305 <naomasa305@gmail.com>
2018-05-10 11:03:34 +0900

    [add]いいね画像読み込みの仮実装

--- resources/assets/js/components/main.vue
+++ resources/assets/js/components/main.vue
@@ -1,31 +1,62 @@
 <template>
     <div>
-        <p>メインなんか</p>
-        <p>Hello {{ $route.params.username }}</p>
+        <p>{{this.user.name}}さんのいいね一覧</p>
         <div v-for="fav in favList">
             {{ fav.text }}
+            {{ fav.extended_entities.media.media_url_https }}
+            <img :src="fav.extended_entities.media[0].media_url_https">
         </div>
+        <br>
     </div>
 </template>
+
+<style>
+    img {
+        max-width: 100px;
+        max-height: 100px;
+    }
+</style>
+
 
 <script>
 export default {
     created() {
         this.getFav();
+        this.loginCheck();
     },
     data() {
         return {
             favList: [],
+            user: null
         }
     },
     methods: {
-        getFav() {
-            var tmp = this;
-            axios.get('fav')
-            .then(function(res) {
+        loginCheck() {
+            axios.get('api/twitterUserCheck')
+            .then(res => {
                 console.log(res.data);
-                tmp.favList = res.data;
+                this.user = res.data;
+                if (this.user === 'notLogin') {
+                    console.log('');
+                }
             })
+        },
+        getFav() {
+            axios.get('api/fav')
+            .then(res => {
+                console.log(res.data);
+                // 画像つきのみを抽出
+                this.favList = res.data.filter(function (elem) {
+                    return(typeof(elem.extended_entities) !== "undefined");
+                });
+            })
+        },
+        tmp () {
+            if (Boolean(fav)) {
+                return true;
+            } else {
+                return false;
+            }
         }
     }
 }
