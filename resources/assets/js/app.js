263e1f3f93301363ea6211a328f3f904c254317c
mnao305 <naomasa305@gmail.com>
2018-05-18 21:11:36 +0900

    [add]vue-masonry導入

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,10 +1,13 @@
 import Vue from 'vue';
 import router from './router';
 import axios from 'axios';
+import { VueMasonryPlugin } from "vue-masonry";
 
 require('./bootstrap');
 
 window.Vue = require('vue');
+
+Vue.use(VueMasonryPlugin);
 
 const app = new Vue({
     router,
