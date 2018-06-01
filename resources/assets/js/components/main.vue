16e4c11150237e545af592f872ef8d87d6163114
mnao305 <naomasa305@gmail.com>
2018-06-01 16:58:02 +0900

    [add]動画表示対応(仮)

--- resources/assets/js/components/main.vue
+++ resources/assets/js/components/main.vue
@@ -3,11 +3,17 @@
         <p>{{user.name}}さんのいいね一覧</p>
         <div v-masonry transition-duration="0.5s" item-selector=".item" class="mainContents">
             <div v-for="fav in favList" class="tweet">
-                <!-- 画像表示部 -->
-                <img v-masonry-tile class="item" :src="fav.extended_entities.media[0].media_url_https">
-                <img v-masonry-tile class="item" v-if="fav.extended_entities.media[1]" :src="fav.extended_entities.media[1].media_url_https">
-                <img v-masonry-tile class="item" v-if="fav.extended_entities.media[2]" :src="fav.extended_entities.media[2].media_url_https">
-                <img v-masonry-tile class="item" v-if="fav.extended_entities.media[3]" :src="fav.extended_entities.media[3].media_url_https">
+                <div v-if="fav.extended_entities.media[0].type == 'video'">
+                    <!-- 動画だったら動画を表示 -->
+                    <video v-masonry-tile class="item" :src="fav.extended_entities.media[0].video_info.variants[1].url" controls></video>
+                </div>
+                <div v-else>
+                    <!-- 画像表示部 -->
+                    <img v-masonry-tile class="item" :src="fav.extended_entities.media[0].media_url_https">
+                    <img v-masonry-tile class="item" v-if="fav.extended_entities.media[1]" :src="fav.extended_entities.media[1].media_url_https">
+                    <img v-masonry-tile class="item" v-if="fav.extended_entities.media[2]" :src="fav.extended_entities.media[2].media_url_https">
+                    <img v-masonry-tile class="item" v-if="fav.extended_entities.media[3]" :src="fav.extended_entities.media[3].media_url_https">
+                </div>
             </div>
         </div>
     </div>
