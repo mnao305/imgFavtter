9a3b47d960423cc9b87379cc4e25070081344f88
mnao305 <naomasa305@gmail.com>
2018-06-19 16:59:40 +0900

    [fix]ナビバーの表示が正常に切り替わらないのを修正

--- resources/assets/js/components/navbar.vue
+++ resources/assets/js/components/navbar.vue
@@ -15,14 +15,14 @@
         <div class="collapse navbar-collapse navbar-ex1-collapse">
             <ul class="nav navbar-nav navbar-right">
                 <li><a href="https://twitter.com/intent/tweet?text=いいねしてきた画像を一覧表示!%20imgFavtter%0a&url=https://imgFavtter.mnao305.com" onClick="window.open(encodeURI(decodeURI(this.href)), 'tweetwindow', 'width=650, height=470, personalbar=0, toolbar=0, scrollbars=1, sizable=1'); return false;" rel="nofollow">ツイート</a></li>
-                <li v-if="user == 'notLogin'"><a href="/login/twitter">登録・ログイン</a></li>
-                <li v-else><router-link to="main">一覧画面へ</router-link></li>
+                <li v-if="login == 'logind'"><router-link to="main">一覧画面へ</router-link></li>
+                <li v-else><a href="/login/twitter">登録・ログイン</a></li>
                 <li class="dropdown">
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">その他<b class="caret"></b></a>
                     <ul class="dropdown-menu">
                         <li><router-link to="about">当アプリについて</router-link></li>
                         <li><router-link to="userconfig">設定</router-link></li>
-                        <li><router-link to="logout">ログアウト</router-link></li>
+                        <li><a href="/logout">ログアウト</a></li>
                     </ul>
                 </li>
             </ul>
@@ -32,18 +32,22 @@
 <script>
 export default {
     created() {
-
+        this.loginCheck();
     },
     data() {
         return {
-            user: null
+            login: null
         }
     },
     methods: {
         loginCheck() {
             axios.get('api/twitterUserCheck')
             .then(res => {
-                this.user = res.data;
+                if (res.data != 'notLogin') {
+                    this.login = 'logind'
+                } else {
+                    this.login = res.data;
+                }
             })
         }
     }
