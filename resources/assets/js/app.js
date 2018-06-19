9191c92198e4137e02387ea6c029fc077d0e10a9
mnao305 <naomasa305@gmail.com>
2018-06-19 20:29:32 +0900

    [add]GA導入

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -2,12 +2,18 @@ import Vue from 'vue';
 import router from './router';
 import axios from 'axios';
 import { VueMasonryPlugin } from "vue-masonry";
+import VueAnalytics from "vue-analytics";
 
 require('./bootstrap');
 
 window.Vue = require('vue');
 
 Vue.use(VueMasonryPlugin);
+
+Vue.use(VueAnalytics, {
+    id: 'UA-57438486-3',
+    router
+  })
 
 const app = new Vue({
     router,
